1130cf21d2fa0417afd303a52860fcdfcf63d68e
Andreas Wittig <andreas@widdix.de>
2019-09-17 09:37:17 +0200

    validating the dashboard name paramater

--- test/test.js
+++ test/test.js
@@ -11,3 +11,16 @@ test('defaults', async t => {
     t.pass();
   }
 });
+
+test('invalid dashboard name', async t => {
+  const stackName = cfntest.stackName();
+  try {
+    t.log(await cfntest.createStack(`${__dirname}/invalid-dashboard-name.yml`, stackName, {}));
+    // what could we test here?
+    t.log(await cfntest.deleteStack(stackName));
+    t.fail();
+  } catch (e) {
+    t.log(await cfntest.deleteStack(stackName));
+    t.pass();
+  }
+});
